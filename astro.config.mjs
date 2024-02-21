8d5394eb4b11dc2572e2226cb866c4b86d11f256
Markus Lewin <lewinmarkus@gmail.com>
2024-02-21 21:13:04 +0100

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
   output: "server",
-  integrations: [react()]
+  adapter: netlify()
 });
